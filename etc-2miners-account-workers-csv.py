4eb4e85de416970189662ccf2965d865b8963e47
Dimych <help-document@yandex.ru>
2019-04-26 00:50:51 +0300

    From home

--- etc-2miners-account-workers-csv.py
+++ etc-2miners-account-workers-csv.py
@@ -8,37 +8,40 @@ url = 'https://etc.2miners.com/api/accounts/0xc53a2409ef3c57eeab564b8a3d66d64ccc
 response = urllib.request.urlopen(url)
 accounts = json.loads(response.read())
 
+curDateTime = datetime.datetime.now().strftime("%d-%m-%Y %H:%M")
+newWorkers = dict()
+
 print(accounts)
 print("===================================")
 
+# TODO Пройтись по словарю, выделить вокеры и считать данные каждого
 print("----------------------")
 print(accounts["workers"])
+print(len(accounts))
 print("----------------------")
-# TODO Пройтись по словарю, выделить вокеры и считать данные каждого
+for key in accounts["workers"]:
+    newWorkers["DateTime"] = curDateTime
+    newWorkers["Rig"] = key
+    newWorkers["lastBeat"]="val_lastBeat"
+    print(key, " - ", accounts["workers"][key])
+
+print(newWorkers)
+print("===================================")
 
 
-curDateTime = datetime.datetime.now().strftime("%d-%m-%Y %H:%M")
 # Получить имя файла
 fileName = datetime.datetime.now().strftime("%d-%m-%Y")+"_workers.csv"
 # Проверить, есть ли файл, если есть, пишем в него, если нет, то создаем новый с полями
 if not os.path.isfile(fileName):
     print("Файла нет!")
     with open(fileName, "a", newline="") as file:
-        columns = ["DateTime", "24hnumreward", "24hreward", "currentHashrate", "hashrate","paymentsTotal","roundShares","workersOffline","workersOnline","workersTotal"]
+        columns = ["DateTime", "Rig", "lastBeat"]
         writer = csv.DictWriter(file, fieldnames=columns)
         writer.writeheader()
 
-newWorkers = dict()
-
-# TODO Сделать заголовки, один раз вводишь в словарь, потом их использовать, вроде словать заголовков
-newWorkers["DateTime"] = curDateTime
-
 # TODO Сделать добавление строк, а не перезапись, но чтобы при добавлении, добавлялись данные, поля должны добавлять для нового файла
 with open(fileName, "+a", newline="") as file:
-    columns = ["DateTime", "24hnumreward", "24hreward", "currentHashrate", "hashrate", "paymentsTotal", "roundShares",
-               "workersOffline", "workersOnline", "workersTotal"]
+    columns = ["DateTime", "Rig", "lastBeat"]
     writer = csv.DictWriter(file, fieldnames=columns)
     writer.writerow(newWorkers)
 
-print(newWorkers)
-print("===================================")
