0ea5bb22d8912364da2747a1be1ff1ab1ac225d0
Dimych <help-document@yandex.ru>
2019-04-28 00:51:13 +0300

    Add work MySQL

---
+++ MySQL.py
@@ -0,0 +1,26 @@
+import mysql.connector
+from mysql.connector import Error
+
+def connect():
+    """ Connect to MySQL database """
+    try:
+        conn = mysql.connector.connect(host='localhost',
+                                       database='test',
+                                       user='root',
+                                       password='82WBu53oYh6o')
+        if conn.is_connected():
+            print('Connected to MySQL database')
+            cursor=conn.cursor()
+            sql = "SELECT * FROM USERS"
+            cursor.execute(sql)
+            result = cursor.fetchone()
+            print(result)
+
+    except Error as e:
+        print(e)
+
+    finally:
+        conn.close()
+
+if __name__ == '__main__':
+    connect()
